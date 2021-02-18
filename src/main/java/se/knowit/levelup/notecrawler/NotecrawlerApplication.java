155585bb4073002c4a5990f2227669a6c31dd37c
Martin Hagelin <martin@hagelin.nu>
2021-02-18 15:59:38 +0100

    Initial commit

---
+++ src/main/java/se/knowit/levelup/notecrawler/NotecrawlerApplication.java
@@ -0,0 +1,13 @@
+package se.knowit.levelup.notecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotecrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotecrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/knowit/levelup/notecrawler/NotecrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.knowit.levelup.notecrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotecrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
